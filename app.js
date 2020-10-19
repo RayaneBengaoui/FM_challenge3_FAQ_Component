71a90d6e338d66bb50b92827a45eb9f05e910ea6
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-10-19 15:32:48 +0200

    Adding dynamic FAQ display

---
+++ app.js
@@ -0,0 +1,13 @@
+questions = document.querySelectorAll(".question-container");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    questions.forEach((q) => {
+      q.children[1].classList.remove("active");
+      q.children[0].children[1].classList.remove("flip");
+    });
+
+    question.children[1].classList.toggle("active");
+    question.children[0].children[1].classList.toggle("flip");
+  });
+});
